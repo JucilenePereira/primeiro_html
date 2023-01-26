241cb006366fe16699a594195dcb1a02e0365f33
Jucilene Pereira <116756482+JucilenePereira@users.noreply.github.com>
2023-01-26 13:23:33 -0300

    atividades adicionadas

---
+++ eventos_teclado/script.js
@@ -0,0 +1,58 @@
+let keyText = document.querySelector("#key-text");
+let codeText = document.querySelector("#code-text");
+let quadrado = document.querySelector("#quadrado");
+let distanciaDaEsquerda = 0
+
+// document.addEventListener("keyup", (e)=> {
+
+//     console.log(e);
+  
+//   })
+
+document.addEventListener("keyup",
+  (e)=> {
+    console.log(e.key);
+    console.log(e.code);
+  })
+
+
+  document.addEventListener("keyup", 
+  (e)=> {
+    keyText.innerText = e.key;
+    codeText.innerText = e.code;
+  })
+
+  document.addEventListener("keyup", 
+  (e)=> {
+    keyText.innerText = e.key;
+    codeText.innerText = e.code;
+  })
+
+  document.addEventListener("keydown", (e) => {
+
+ 
+
+  })
+
+  	
+document.addEventListener("keydown", (e) => {
+    if(e.code == "ArrowRight"){
+      console.log("Apertou a seta à direita");
+    }
+  })
+
+  document.addEventListener("keydown", (e) => {
+    if(e.code == "ArrowRight"){
+      distanciaDaEsquerda = distanciaDaEsquerda + 10
+      console.log(distanciaDaEsquerda);
+    }
+  })
+
+  document.addEventListener("keydown", (e) => {
+    if(e.code == "ArrowRight"){
+      distanciaDaEsquerda = distanciaDaEsquerda + 10
+      console.log(distanciaDaEsquerda);
+  
+      quadrado.style.left = distanciaDaEsquerda + "px"
+    }
+  })
